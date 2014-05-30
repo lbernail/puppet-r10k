20d575012eea1f6abfe22dddc1710b642e68f708
Laurent Bernaille <laurent.bernaille@gmail.com>
2014-05-30 17:27:05 +0200

    set up test gpg data in hiera and message in default node

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,8 @@
 node 'default' {
-      notify { 'Default class for unknown node': }
+  $message=hiera('common::message::gpg','No such key')
+
+  notify { 'Default class for unknown node': }
+  -> notify { "GPG test message: ${message}": }
 }
 
 node 'puppetmaster' {
