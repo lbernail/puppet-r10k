db9747c330b29b57c7e1e12e718461d944d4ed72
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-09-18 11:04:22 +0200

    Fix ssl parameters

--- site/profile/manifests/puppetreports.pp
+++ site/profile/manifests/puppetreports.pp
@@ -23,14 +23,15 @@ class profile::puppetreports(
   }
 
   $ssl_dir = $::settings::ssldir
-  $puppetboard_certname = $::certname
+  $puppetboard_certname = $clientcert
+
   class { 'puppetboard':
     groups              => 'puppet',
     manage_virtualenv   => true,
     puppetdb_host       => hiera('puppetdb_host'),
     puppetdb_port       => '8081',
     puppetdb_key        => "${ssl_dir}/private_keys/${puppetboard_certname}.pem",
-    puppetdb_ssl_verify => true,
+    puppetdb_ssl_verify => false,
     puppetdb_cert       => "${ssl_dir}/certs/${puppetboard_certname}.pem",
   }
 
